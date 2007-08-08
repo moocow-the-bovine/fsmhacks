03ed48851400ddd1911cd459e7241f9fd98bba52
Bryan Jurish <jurish@bbaw.de>
2007-08-08 09:37:41 +0000

    *** empty log message ***
    git-svn-id: file:///home/moocow/svn/private/fsmhacks/trunk@2077 0ab4dd9e-c427-11dc-aa3c-0bd9649fc1d2

--- fsm2gfsm.sh
+++ fsm2gfsm.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-. `dirname $0/fsmfuncs.sh`
+. `dirname $0`/fsmfuncs.sh
 if test -n "$1" -a "$1" != "-" ; then
   afsm="$1"
   shift
-  flags=`fsm2gfsm_compile_flags "$afsm"`
 else
-  ##-- reading from a pipe: expect that gfsm flags are given on command-line
-  ( echo "$0: WARNING: cannot auto-determine (acceptor|transducer) status for STDIN" >&2 )
+  ##-- reading from a pipe: create a tempfile
   if test "$1" = "-" ; then
     afsm="$1";
     shift;
   else
     afsm="-"
   fi
-  flags=""
+  tmp=`fsm_tempfile fsm2gfsm .afsm`
+  cat "$afsm" > $tmp
+  afsm="$tmp"
 fi
 
+flags=`fsm2gfsm_compile_flags "$afsm"`
 exec fsmprint "$afsm" | gfsmcompile $flags "$@"
