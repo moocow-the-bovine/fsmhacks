9af7e735f2d0047f4d9db4f6e49622b90ba89050
Bryan Jurish <jurish@bbaw.de>
2007-08-14 08:07:04 +0000

    *** empty log message ***
    git-svn-id: file:///home/moocow/svn/private/fsmhacks/trunk@2088 0ab4dd9e-c427-11dc-aa3c-0bd9649fc1d2

--- dotgv.sh
+++ dotgv.sh
@@ -5,9 +5,41 @@ DOT="dot"
 DOTFLAGS="-Tps -Gcharset=latin1"
 
 GV="gv"
-GVFLAGS="--spartan --orientation=landscape --noantialias"
+#GVFLAGS="--spartan --orientation=landscape --noantialias"
 
 PSFILE=`fsm_tempfile dotgv .ps`
+
+##-- get GVFLAGS
+if test -z "$GVFLAGS" ; then
+  GV_OPT_VERSION="`$GV -version 2>/dev/null`"
+  GV_OPT_V="`$GV -v 2>/dev/null`"
+  
+  if test -n "$GV_OPT_V" -a -n "$GV_OPT_VERSION" ; then
+    if echo "$GV_OPT_VERSION" | grep Usage ; then
+      GV_VERSION="$GV_OPT_V"
+    else
+      GV_VERSION="$GV_OPT_VERSION"
+    fi
+  elif test -n "$GV_OPT_V" ; then
+    GV_VERSION="$GV_OPT_V"
+  elif test -n "$GV_OPT_VERSION" ; then
+    GV_VERSION="$GV_OPT_VERSION"
+  else
+    GV_VERSION="???"
+  fi
+  #echo "GV_VERSION=$GV_VERSION" >&2
+  
+  GVFLAGS_COMMON="-spartan -noantialias"
+  case "$GV_VERSION" in
+    *\ 3.5.*)
+      GVFLAGS="-landscape $GVFLAGS_COMMON"
+      ;;
+    *)
+      ##-- e.g. 3.6.3
+      GVFLAGS="-orientation=landscape $GVFLAGS_COMMON"
+      ;;
+  esac
+fi
 
 mode="dot"
 for arg in $* ; do
