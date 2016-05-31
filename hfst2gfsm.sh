85997df69160d3125e55444db8b3f641a15c2da1
Bryan Jurish <jurish@bbaw.de>
2016-05-31 12:20:56 +0000

    git-svn-id: file:///home/moocow/svn/private/fsmhacks/trunk@8331 0ab4dd9e-c427-11dc-aa3c-0bd9649fc1d2

--- hfst2gfsm.sh
+++ hfst2gfsm.sh
@@ -5,7 +5,7 @@
 hfst=""
 dummy=""
 keep=""
-brackets=""
+xbrackets="y"
 
 ##==============================================================
 ## Subs
@@ -15,11 +15,11 @@ show_usage() {
 Usage: $0 [OPTIONS] HFST
 
 Options:
-   -h , -help       # this help message
-   -o , -out BASE   # set output basename (default=\${HFST%.hfst*})
-   -d , -dummy      # just print commands
-   -b , -brackets   # keep bracketed symbols in lab files? (default=map [X] to _X)
-   -k , -keep       # keep temporary tfst? (default=no)
+   -h     , -help         # this help message
+   -o     , -out BASE     # set output basename (default=\${HFST%.hfst*})
+   -d     , -dummy        # just print commands
+   -[no]b , -[no]brackets # do/don't translate bracketed symbols in lab files? (default=do)
+   -[no]k , -[no]keep     # keep temporary tfst? (default=don't)
 
 EOF
 }
@@ -42,22 +42,25 @@ die() {
 ## MAIN
 while test $# -gt 0; do
   case "$1" in
-    ""|"-h"|"-help"|"--help")
-	show_usage;
-	exit 0;
-	;;
-    "-d"|"-dummy"|"-dry-run")
-	dummy=y
-	;;
-    "-k"|"-keep")
-	keep=y
-	;;
-    "-b"|"-brackets")
-	brackets=y
-	;;
-    *)
-	hfst="$1"
-	;;
+      ""|"-h"|"-help"|"--help")
+	  show_usage;
+	  exit 0;
+	  ;;
+      "-d"|"-dummy"|"-dry-run") dummy=y ;;
+      "-k"|"-keep") keep=y ;;
+      "-nok"|"-nokeep") keep="" ;;
+      "-b"|"-brackets"|"-xb"|"-xlate-brackets") xbrackets=y; ;;
+      "-nob"|"-nobrackets"|"-noxb"|"-noxlate-brackets") xbrackets=""; ;;
+#      "-t"|"-tags"|"-xt"|"-xlate-tags") xtags=y; ;;
+#      "-not"|"-notags"|"-noxt"|"-noxlate-tags") xtags=""; ;;
+#      "-f"|"-feat"|"-features"|"-xf"|"-xlate-features") xfeat=y; ;;
+#      "-nof"|"-nofeat"|"-nofeatures"|"-noxf"|"-noxlate-features") xfeat=""; ;;
+      "-"*)
+	  die "unknown option '$1'"
+	  ;;
+      *)
+	  hfst="$1"
+	  ;;
   esac
   shift
 done
@@ -74,10 +77,11 @@ runcmd hfst-fst2txt --output="$obase.tfst" "$hfst" \
     || die "hfst-fst2txt failed"
 
 ##-- hack brackets labels?
-if test -z "$brackets"; then
-    runcmd perl -i -pe 's/(\s)\[([A-Z]+)\](\s)/$1_$2$3/sg; s/(\s)([\+\/A-Z]{2,})(\s)/$1.lc($2).$3/esg;' "$obase.tfst" \
-	   || die "bracket replacement failed"
+if test -n "$xbrackets"; then
+    runcmd perl -i -pe 's/(?<=\s)\[(\S+)\](?=\s)/<$1>/sg;' "$obase.tfst" \
+	   || die "bracket-label hack failed"
 fi
+
 
 ##-- generate labels
 evalcmd "echo -e '@0@\\t0' > '$obase.lab'"
