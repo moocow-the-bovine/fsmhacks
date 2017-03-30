0d02ad16fe73b6d6f1a3823cc18e0fc44c5b17e6
Bryan Jurish <jurish@bbaw.de>
2017-03-30 08:29:54 +0000

    git-svn-id: file:///home/moocow/svn/private/fsmhacks/trunk@8808 0ab4dd9e-c427-11dc-aa3c-0bd9649fc1d2

--- gfsmlookup-strings.sh
+++ gfsmlookup-strings.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 show_help() {
-  echo "Usage: $0 [-u|--utf8] LABFILE STRING GFSMFILE" >&2
+    cat <<EOF >&2
+Usage: $0 [OPTIONS] LABFILE STRING GFSMFILE
+
+Options:
+   -u, --utf8    # assume UTF-8 alphabet
+   -a, --att     # honor AT&T input escapes
+   -A, --align   # output aligned arc paths
+
+EOF
 }
 
 args=()
 opts=()
-utf8=()
+labopts=()
+stropts=(-a)
 while [ $# -gt 0 ] ; do
     case "$1" in
 	-h|-help|--help)
@@ -14,7 +23,15 @@ while [ $# -gt 0 ] ; do
 	    exit 0
 	    ;;
 	-u|-utf8|--utf8)
-	    utf8=(--utf8)
+	    labopts[${#labopts[@]}]="-u"
+	    stropts[${#stropts[@]}]="-u"
+	    ;;
+	-a|-att|--att)
+	    labopts[${#labopts[@]}]="-a"
+	    #stropts[${#stropts[@]}]="-a"
+	    ;;
+	-A|-align|--align)
+	    stropts[${#stropts[@]}]="-A"
 	    ;;
 	*)
 	    if [ ${#args[@]} -lt 3 ]; then
@@ -38,8 +55,8 @@ labfile="${args[0]}"
 string="${args[1]}"
 fsmfile="${args[2]}"
 
-gfsmlookup -f "$fsmfile" `echo "$string" | gfsmlabels "${utf8[@]}" -l "$labfile"` \
-| gfsmstrings "${utf8[@]}" -i "$labfile" -o "$labfile" -a
+gfsmlookup -f "$fsmfile" `echo "$string" | gfsmlabels "${labopts[@]}" -l "$labfile"` \
+| gfsmstrings "${stropts[@]}" -i "$labfile" -o "$labfile"
 
 #| fsmstringsort.perl
 ##--
